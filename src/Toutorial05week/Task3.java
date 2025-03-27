d65661257b38d22c8faf1ba3d67756c35ca0c3c1
Unish Bhattarai <unishbhattarai2062@gmail.com>
2025-03-27 08:38:15 +0545

    add more practice

--- src/Toutorial05week/Task3.java
+++ src/Toutorial05week/Task3.java
@@ -1,4 +1,38 @@
 package Toutorial05week;
 
+abstract class shape {
+    public void getrectanglearea( int length, int breadth) {
+
+    }
+    public void getsquarearea( int length0) {
+
+    }
+    public void getcirclearea(int radius ) {
+
+    }
+}
+
+
+class area extends shape {
+    @Override
+    public void getrectanglearea(int length, int breadth) {
+        System.out.println("Area of rectangle is " + length * breadth);
+    }
+
+    public void getsquarearea(int length) {
+        System.out.println("Area of square is " + length * length);
+    }
+
+    public void getcirclearea(int radius) {
+        System.out.println("Area of circle is " + Math.PI * radius * radius);
+    }
+}
+
 public class Task3 {
+    public static void main(String[] args) {
+        area obj1 = new area();
+        obj1.getrectanglearea(5,6);
+        obj1.getsquarearea(5);
+        obj1.getcirclearea(5);
+    }
 }
