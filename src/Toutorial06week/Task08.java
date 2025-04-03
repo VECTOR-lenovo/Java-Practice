ab275557541222699150a9c48ee30c1a534b682a
Unish Bhattarai <unishbhattarai2062@gmail.com>
2025-04-03 08:53:52 +0545

    add practice

---
+++ src/Toutorial06week/Task08.java
@@ -0,0 +1,13 @@
+package Toutorial06week;
+import java.util.HashSet;
+public class Task08 {
+    public static void main(String[] args) {
+        HashSet<Integer> set1 = new HashSet<Integer>();
+        set1.add(1 );
+        set1.add(2);
+        set1.add(3);
+        set1.add(2);
+        System.out.println(set1);
+
+    }
+}
