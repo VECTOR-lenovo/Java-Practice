7fabbefb21163bf598a6b972b8797fa1a00508ab
Unish Bhattarai <unishbhattarai2062@gmail.com>
2025-04-03 08:57:12 +0545

    add practice

---
+++ src/Toutorial06week/Task09.java
@@ -0,0 +1,13 @@
+package Toutorial06week;
+import java.util.TreeSet;
+public class Task09 {
+    public static void main(String[] args) {
+        TreeSet<String> color = new TreeSet<String>();
+        color.add("Blue");
+        color.add("Red");
+        color.add("Green");
+        color.add("Purple");
+        System.out.println(color);
+
+    }
+}
