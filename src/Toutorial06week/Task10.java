0c19c274147a4d4d22e9ab68f2a0c878818d8d04
Unish Bhattarai <unishbhattarai2062@gmail.com>
2025-04-03 08:59:30 +0545

    add practice

---
+++ src/Toutorial06week/Task10.java
@@ -0,0 +1,13 @@
+package Toutorial06week;
+import java.util.TreeSet;
+public class Task10 {
+    public static void main(String[] args) {
+        TreeSet<Integer> set1 = new TreeSet<Integer>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        System.out.println(set1.descendingSet());
+
+    }
+}
